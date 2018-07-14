db01a668aa502acbff0ad6ae37848fe60575c02e
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-14 10:41:56 +1000

    Adding mysql sleep workaround

--- tests/web_tests.sh
+++ tests/web_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 sleep 1
 echo Executing web tests
+
+while ! mysqladmin ping -h 127.0.0.1 --silent; do
+    echo MySQL is not ready... sleeping...
+    sleep 1
+done
+
 for i in {1..4}
 do
-    docker ps
-    docker logs sugar-mysql
     OUTPUT=`curl -s http://docker.local/sugar/$i.php | grep ok | wc -l`
     if [ $OUTPUT != '1' ]
     then
