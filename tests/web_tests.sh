d0443f6a553cbf888510743219483d0c4e25c085
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-16 16:48:25 +1000

    Third attempt

--- tests/web_tests.sh
+++ tests/web_tests.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 echo Executing web tests
 
+MAX=60
+
 while ! mysqladmin ping -h 127.0.0.1 --silent; do
     echo MySQL is not ready... sleeping...
     sleep 2
+    $MAX -= 2
+    if [ $MAX -le 0]
+    then
+        echo Maximum MySQL timeout reached
+        exit 1
+    fi
 done
 
 for i in {1..4}
