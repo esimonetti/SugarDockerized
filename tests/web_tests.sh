eb744e19842f7bf25a1f683d21b6863baae57457
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-16 16:55:32 +1000

    Decrementing variable

--- tests/web_tests.sh
+++ tests/web_tests.sh
@@ -6,7 +6,7 @@ MAX=60
 while ! mysqladmin ping -h 127.0.0.1 --silent; do
     echo MySQL is not ready... sleeping...
     sleep 2
-    MAX=$MAX - 2
+    MAX=$((MAX - 2))
     if [ $MAX -le 0 ]
     then
         echo Maximum MySQL timeout reached
