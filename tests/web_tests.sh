42f64cbe4ea33482ca858960dbf44b45eac5944c
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-17 19:37:27 +1000

    Attempt to debug the timeout

--- tests/web_tests.sh
+++ tests/web_tests.sh
@@ -6,7 +6,7 @@ echo Executing web tests
 MAX=120
 INTERVAL=5
 MAX_MYSQL=$MAX
-MAX_ELASTIC=$MAX
+MAX_ELASTIC=600
 
 while [ `./utilities/runcli.sh "(echo >/dev/tcp/sugar-mysql/3306) &>/dev/null && echo 1 || echo 0"` != "1" ] ; do
     echo MySQL is not ready... sleeping...
@@ -22,6 +22,7 @@ done
 while [ `./utilities/runcli.sh "(echo >/dev/tcp/sugar-elasticsearch/9200) &>/dev/null && echo 1 || echo 0"` != "1" ] ; do
     echo Elasticsearch is not ready... sleeping...
     sleep $INTERVAL
+    docker ps
     MAX_ELASTIC=$((MAX_ELASTIC - $INTERVAL))
     if [ $MAX_ELASTIC -le 0 ]
     then
