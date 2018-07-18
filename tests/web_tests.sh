3b00f3dd9e8a05f7d65b1c042d8444c7f70c28aa
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-18 14:36:04 +1000

    Finalised script to run also CLI version of the scripts

--- tests/web_tests.sh
+++ tests/web_tests.sh
@@ -6,7 +6,8 @@ else
     echo Installing composer dependencies if any
     ./utilities/runcli.sh "cd ./web_tests/$3/ && composer install"
 
-    echo Executing web tests
+    echo Confirming that MySQL and Elasticsearch are available
+
     MAX=60
     INTERVAL=5
     MAX_MYSQL=$MAX
@@ -34,17 +35,32 @@ else
         fi
     done
 
+    echo Executing CLI and web tests
+
     for i in {1..5}
     do
-        OUTPUT=`curl -s http://docker.local/sugar/web_tests/$3/test_$i.php | grep ok | wc -l`
-        if [ $OUTPUT != '1' ]
+        OUTPUT_CLI=`./utilities/runcli.sh "php ./web_tests/$3/test_$i.php | grep ok | wc -l"`
+        if [ $OUTPUT_CLI != '1' ]
         then
-            echo Error for script test_$i.php
-            echo Retrieving logs from the web server:
+            echo Error for CLI script test_$i.php
+            echo Completing a syntax check for script ./web_tests/$3/test_$i.php
+            echo `./utilities/runcli.sh "php -l ./web_tests/$3/test_$i.php"`
+            echo Retrieving complete logs from the Cron CLI server:
+            docker logs sugar-cron
+            exit 1
+        else
+            echo Script ./web_tests/$3/test_$i.php executed successfully via CLI
+        fi
+
+        OUTPUT_WEB=`curl -s http://docker.local/sugar/web_tests/$3/test_$i.php | grep ok | wc -l`
+        if [ $OUTPUT_WEB != '1' ]
+        then
+            echo Error for web script test_$i.php
+            echo Retrieving complete logs from the web server:
             docker logs sugar-web1
             exit 1
         else
-            echo Script test_$i.php executed successfully
+            echo Script http://docker.local/sugar/web_tests/$3/test_$i.php executed successfully via web
         fi
     done
 fi
