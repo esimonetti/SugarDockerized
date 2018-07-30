d4f034d68ef3f29bc0e13baea4228ac883745d96
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-07-30 17:47:27 +1000

    stop.sh parameter (#36)
    * Added docker hub pulls image
    
    * Changed slightly readme
    
    * Readme
    
    * README
    
    * README
    
    * README
    
    * Fixing up parameter number for stop.sh

--- tests/stop.sh
+++ tests/stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -z ${STACK} ]
+if [ -z $1 ]
 then
     echo The script needs the stack as a parameter
     exit 1
 else
-    echo Stopping environment ${STACK}
-    docker-compose -f ${STACK} down
+    echo Stopping environment $1
+    docker-compose -f $1 down
 fi
