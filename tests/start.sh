d95f05c3abcee21d383b2e840c0167dcfa30d3f5
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-11-21 19:53:06 +1100

    Removed once again setownership for testing purposes

--- tests/start.sh
+++ tests/start.sh
@@ -4,8 +4,8 @@ then
     echo The script needs the stack as a parameter
     exit 1
 else
-    echo Setting correct directory ownership
-    ./utilities/setownership.sh
+    #echo Setting correct directory ownership
+    #./utilities/setownership.sh
     echo Starting environment $1
     docker-compose -f $1 up -d --build
 fi
