6621bf2329e6c45f62c8aeac4b223ff4f61f8e41
Enrico Simonetti <enrico.simonetti@gmail.com>
2019-12-23 18:39:10 +1100

    Added elastic change for permissions

--- utilities/setownership.sh
+++ utilities/setownership.sh
@@ -17,22 +17,22 @@ then
     then
         # get apache ownership
         ownership=($(docker exec -it sugar-cron cat /etc/passwd | grep sugar | awk '{ split($0, out, ":"); print out[3],out[4] }'))
-        echo Setting from within the sugar-cron container, the ownership of /var/www/html/sugar to ${ownership[0]}:${ownership[1]}
+        echo Setting from within the sugar-cron container, the ownership of /var/www/html/sugar to sugar:sugar \(${ownership[0]}:${ownership[1]}\)
         docker exec -it sugar-cron sudo chown -R ${ownership[0]}:${ownership[1]} /var/www/html/sugar
 
         # get mysql ownership
         ownership=($(docker exec -it sugar-mysql cat /etc/passwd | grep mysql | awk '{ split($0, out, ":"); print out[3],out[4] }'))
-        echo Setting from within the sugar-mysql container, the ownership of /var/lib/mysql to ${ownership[0]}:${ownership[1]}
+        echo Setting from within the sugar-mysql container, the ownership of /var/lib/mysql to mysql:mysql \(${ownership[0]}:${ownership[1]}\)
         docker exec -it sugar-mysql chown -R ${ownership[0]}:${ownership[1]} /var/lib/mysql
 
-        # get elastic ownership
-        ownership=($(docker exec -it sugar-elasticsearch cat /etc/passwd | grep elastic | awk '{ split($0, out, ":"); print out[3],out[4] }'))
-        echo Setting from within the sugar-elasticsearch container, the ownership of /usr/share/elasticsearch/data to ${ownership[0]}:${ownership[1]}
-        docker exec -it sugar-elasticsearch chown -R ${ownership[0]}:${ownership[1]} /usr/share/elasticsearch/data
+        # get elastic ownership (group is root)
+        ownership=($(docker exec -it sugar-elasticsearch cat /etc/passwd | grep elasticsearch | awk '{ split($0, out, ":"); print out[3],out[4] }'))
+        echo Setting from within the sugar-elasticsearch container, the ownership of /usr/share/elasticsearch/data to elasticsearch:root \(${ownership[0]}:0\)
+        docker exec -it sugar-elasticsearch chown -R ${ownership[0]}:0 /usr/share/elasticsearch/data
 
         # get redis ownership
         ownership=($(docker exec -it sugar-redis cat /etc/passwd | grep redis | awk '{ split($0, out, ":"); print out[3],out[4] }'))
-        echo Setting from within the sugar-redis container, the ownership of /data to ${ownership[0]}:${ownership[1]}
+        echo Setting from within the sugar-redis container, the ownership of /data to redis:redis \(${ownership[0]}:${ownership[1]}\)
         docker exec -it sugar-redis chown -R ${ownership[0]}:${ownership[1]} /data
     else
         echo Please start the relevant stack first
